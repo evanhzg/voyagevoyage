e3ead932a0aee68036f75d1ca5b45159fbc3b38e
Sajedeh Adelia Fathipour <adelia.fathipour@gmail.com>
2022-10-07 11:51:45 +0200

    test de too tooo

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -9,17 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\City;
 use App\Repository\CityRepository;
-//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-//use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
-//use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Faker\Generator;
 use Faker\Factory;
+// tooo tooooo
 
 class CityController extends AbstractController
 {
