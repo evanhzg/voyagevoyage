46f076573693b4fdb232a1ad1f4bbc1620b3a01c
L.D <61159151+Roarkiner@users.noreply.github.com>
2022-11-12 20:44:19 +0100

    Right bundle added

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
 ];
